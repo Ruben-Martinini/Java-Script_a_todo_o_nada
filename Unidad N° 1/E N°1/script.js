7f7ea0d264fbbf8873a49e86d430bde9ff74da20
Ruben <rubariel78@gmail.com>
2022-02-27 20:22:28 -0300

    n1

---
+++ Unidad N° 1/E N°1/script.js
@@ -0,0 +1,14 @@
+/* 
+1- Deberán mostrar por consola los siguientes datos:
+a) Mostrar el día de tu cumpleaños (en numero) Ej: 23
+b) Mostrar el mes de tu cumpleaños (en numero) Ej: 07
+c) Mostrar el año de tu nacimiento.
+d) Mostrar tu número favorito.
+*/
+
+console.log("Mi cumpleaños es el día: " + 9);
+console.log("Mi cumpleaños es el mes: " + 2);
+console.log("El año de mi nacimiento es: " + 1978);
+console.log("mi número favorito es el: " + 23);
+
+
